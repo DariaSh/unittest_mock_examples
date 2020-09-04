62f6117cc9be025f60e83de277877c58ae5a3ba2
daria <danguyen.ext@galerieslafayette.com>
2020-09-04 12:03:02 +0200

    updated example with alterntive calls testing

--- my_module.py
+++ my_module.py
@@ -2,5 +2,14 @@ from utils.bucket_reader import read, get_bucket
 
 
 def my_method(bucket_name, blob_name):
-  bucket_to_read = get_bucket(bucket_name)
-  return read(bucket_to_read, blob_name)
+    bucket_to_read = get_bucket(bucket_name)
+    return read(bucket_to_read, blob_name)
+
+
+def my_other_method(bucket):
+    bucket_name_prefix = bucket.get_prefix()
+    bucket_name_1 = bucket_name_prefix + "_name_1"
+    b1 = get_bucket(bucket_name_1)
+    bucket_name_2 = bucket_name_prefix + "_name_2"
+    b2 = get_bucket(bucket_name_2)
+    return b1, b2
--- test_my_module.py
+++ test_my_module.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
-from my_module import my_method
+from unittest.mock import patch, MagicMock, call
+from my_module import my_method, my_other_method
 
 
 class MyTest(TestCase):
@@ -20,3 +20,35 @@ class MyTest(TestCase):
         mock_get_bucket.assert_called_once_with(bucket_name)
         mock_read.assert_called_once_with(mock_bucket, blob_name)
         self.assertEqual(expected, result)
+
+    @patch('my_module.get_bucket')
+    def test_my_other_method(self, mock_get_bucket):
+        prefix = "my_bucket"
+        mock_bucket = MagicMock()
+        mock_bucket.get_prefix.return_value = prefix
+        bucket_name_1 = "_name_1"
+        bucket_name_2 = "_name_2"
+        bucket_names = [prefix + bucket_name_1, prefix + bucket_name_2]
+        my_other_method(mock_bucket)
+
+        method_call_count = 2
+        self.assertEqual(method_call_count, mock_get_bucket.call_count)
+        for i in range(method_call_count):
+            self.assertEqual(call(bucket_names[i]), mock_get_bucket.mock_calls[i])
+
+    @patch('my_module.get_bucket')
+    def test_my_other_method_as_list(self, mock_get_bucket):
+        prefix = "my_bucket"
+        mock_bucket = MagicMock()
+        mock_bucket.get_prefix.return_value = prefix
+        bucket_name_1 = "_name_1"
+        bucket_name_2 = "_name_2"
+        bucket_names = [prefix + bucket_name_1, prefix + bucket_name_2]
+        expected_mock_calls = []
+
+        for bucket_name in bucket_names:
+            expected_mock_calls.append(call(bucket_name))
+
+        my_other_method(mock_bucket)
+
+        self.assertListEqual(expected_mock_calls, mock_get_bucket.mock_calls)
