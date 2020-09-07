a1769a9d22f53f0d0d3411c3d900d6c3bc2adb9c
daria <danguyen.ext@galerieslafayette.com>
2020-09-07 09:23:16 +0200

    core formatting updated

--- my_module.py
+++ my_module.py
@@ -1,12 +1,13 @@
+from bucket import Bucket
 from utils.bucket_reader import read, get_bucket
 
 
-def my_method(bucket_name, blob_name):
+def my_method(bucket_name: str, blob_name: str) -> str:
     bucket_to_read = get_bucket(bucket_name)
     return read(bucket_to_read, blob_name)
 
 
-def my_other_method(bucket):
+def my_other_method(bucket: Bucket) -> tuple:
     bucket_name_prefix = bucket.get_prefix()
     bucket_name_1 = bucket_name_prefix + "_name_1"
     b1 = get_bucket(bucket_name_1)
