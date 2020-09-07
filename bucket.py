19ca2b4c13f3ff972528dee9262a7fd079c053b9
daria <danguyen.ext@galerieslafayette.com>
2020-09-07 09:25:27 +0200

    core formatting updated

--- bucket.py
+++ bucket.py
@@ -4,7 +4,14 @@ from google.cloud.storage import Client
 class Bucket:
 
     def __init__(self, working_project: str):
+        self.prefix = ''
         self.storage_client = Client(project=working_project)
 
     def get_bucket(self, bucket_name: str):
         return self.storage_client.get_bucket(bucket_name)
+
+    def set_prefix(self, prefix):
+        self.prefix = prefix
+
+    def get_prefix(self) -> str:
+        return self.prefix
