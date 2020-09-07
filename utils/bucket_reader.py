d0426bd3cfa8e800f7d671a39007dde1d0973135
daria <danguyen.ext@galerieslafayette.com>
2020-09-07 09:18:49 +0200

    updated example with realistic code

--- utils/bucket_reader.py
+++ utils/bucket_reader.py
@@ -1,8 +1,14 @@
+from google.cloud import storage
+from bucket import Bucket
 
 
-def get_bucket(bucket_name):
-    return "some bucket"
+def get_bucket(bucket_name: str) -> Bucket:
+    # instantiating object Bucket here
+    return Bucket("some bucket")
 
 
-def read(bucket_to_read, blob_name):
-    return "some blob content"
+def read(bucket_to_read: Bucket, blob_name: str) -> str:
+    # reading content of some file stored in a GCP bucket and returning in a form of string
+    blob = storage.Blob(blob_name)
+    result = blob.download_as_string(client=bucket_to_read.client)
+    return result
